262c72d9ee85d4b197eaa7e919fa38f2b8dccf00
corantoine <158170889+corantoine@users.noreply.github.com>
2024-06-04 13:48:08 +0200

    tâche terminée passe à true et le css s'applique

---
+++ src/main/javascript/components/Task.js
@@ -0,0 +1,27 @@
+// export default function Taches(props) {
+    // const taskInfo = props.taskInfo;
+    // const onTaskDelete = props.onTaskDelete;
+    // const onTaskCheck = props.onTaskCheck;
+// Peut aussi s'écrire comme ça (destructuring): 
+// const {taskInfo, onTaskCheck, onTaskDelete} = props
+// passer props en params de la fonction revient au même que de faire le destructuring directement en params de la fonction
+
+import { useState } from "react"
+
+    export default function Task({taskInfo, onTaskDelete, onCheck, onTaskCompleted}) {
+    return ( 
+        <li 
+        className={onTaskCompleted ? "completed-task" : "uncomplete-task"}>
+            {taskInfo.nom}
+            <input 
+            className="checkbox"
+            type="checkbox"
+            onChange={() => onCheck(taskInfo.id)}
+            checked={taskInfo.taskCompleted}
+            />            
+            <button
+                className="deleteButton"
+                onClick={() => onTaskDelete(taskInfo.id)}>X</button>
+        </li>
+    )
+}
